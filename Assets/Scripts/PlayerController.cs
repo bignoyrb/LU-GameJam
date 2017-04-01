e90b913d69972e2075351edf15993ae6369f7a77
Patrick Joanis <pjoanis@lakeheadu.ca>
2017-04-01 14:27:52 -0400

    added animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,22 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;    
 	private Rigidbody2D rb2d;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
     public Rigidbody2D projectile;
     private float lastShotTime;
 
     void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         lastShotTime = Time.time;
 	}
 
     void Update()
     {
-        double firingTime = 1.0;        
+        double firingTime = 0.3;        
         float shootHorizontal = Input.GetAxis("Fire1");
         float shootVertical = Input.GetAxis("Fire2");
 
@@ -44,6 +48,32 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
         rb2d.velocity = movement*speed;
+        // direction: 0->idle, 1->down, 2->right, 3->up, 4->left
+        int direction;
+        int previousDirection = animator.GetInteger("Direction");
+        if (moveHorizontal == 0 && moveVertical == 0)
+            direction = 0;
+        else if (Mathf.Abs(moveHorizontal) > Mathf.Abs(moveVertical))
+        {
+            if (moveHorizontal > 0)           
+                direction = 2;
+            
+            else
+                direction = 4;
+        }
+        else
+        {
+            if (moveVertical > 0)
+                direction = 3;
+            else
+                direction = 1;
+        }
+        if (direction == 2)
+            spriteRenderer.flipX = true;
+        else
+            spriteRenderer.flipX = false;
+        animator.SetInteger("PreviousDirection", previousDirection);
+        animator.SetInteger("Direction",direction);        
 	}
 		
 	void OnTriggerEnter2D(Collider2D other)
