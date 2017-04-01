dac29deab349a72d25a7ae16d91a3809ace3811c
Patrick Joanis <pjoanis@lakeheadu.ca>
2017-04-01 12:54:59 -0400

    shooting stuff

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -6,7 +6,9 @@ public class BulletScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        // lifetime of bullet in seconds
+        int lifeTime = 1;
+        Destroy(this.gameObject,(float)1.0);
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,27 +6,38 @@ public class PlayerController : MonoBehaviour {
 	public float speed;    
 	private Rigidbody2D rb2d;
     public Rigidbody2D projectile;
+    private float lastShotTime;
 
     void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
+        lastShotTime = Time.time;
 	}
 
     void Update()
     {
-        float bulletSpeed = 10;
+        double firingTime = 1.0;        
         float shootHorizontal = Input.GetAxis("Fire1");
         float shootVertical = Input.GetAxis("Fire2");
+
         if (Mathf.Abs(shootHorizontal)>0.1 || Mathf.Abs(shootVertical)>0.1)
-        {            
-            Rigidbody2D instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation);
-            // get normalized shooting direction
-            Vector2 shootingDirection = new Vector2(shootHorizontal, shootVertical);
-            shootingDirection.Normalize();
-            instantiatedProjectile.velocity = shootingDirection*bulletSpeed;
+        {
+            if (Time.time > (firingTime + lastShotTime))
+            {
+                // get normalized shooting direction
+                Vector2 shootingDirection = new Vector2(shootHorizontal, shootVertical);
+                shootingDirection.Normalize();
+                Fire(shootingDirection);
+                lastShotTime = Time.time;
+            }
         }
     }
-
+    void Fire(Vector2 shootingDirection)
+    {
+        float bulletSpeed = 10;
+        Rigidbody2D instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation);
+        instantiatedProjectile.velocity = shootingDirection * bulletSpeed;
+    }
 	void FixedUpdate()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -37,9 +48,9 @@ public class PlayerController : MonoBehaviour {
 		
 	void OnTriggerEnter2D(Collider2D other)
 	{		
-		if (other.gameObject.CompareTag ("PickUp")) 
-		{
-			other.gameObject.SetActive (false);
-		}
+		//if (other.gameObject.CompareTag ("PickUp")) 
+		//{
+		//	other.gameObject.SetActive (false);
+		//}
 	}
 }
